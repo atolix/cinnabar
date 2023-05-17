6de04186d075e1ed3cfdecf1e5abf6dfa7fc5ae2
atolix <82761106+atolix@users.noreply.github.com>
2023-05-17 22:23:05 +0900

    add mod tokenizer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+mod tokenizer;
+use tokenizer::tokenize;
+
 fn main() {
-    println!("Hello, world!");
+    let input = "2 + 3 * 4 - (1 + 2) / 3";
+    let tokens = tokenize(input);
+    println!("{:?}", tokens);
 }
