bb66d37bb4f93df0fcb028866c73a28a72dd486d
atolix <82761106+atolix@users.noreply.github.com>
2023-05-28 20:34:14 +0900

    minus

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -36,7 +36,24 @@ pub fn tokenize(input: &str) -> Result<Vec<Token>, String> {
             }
             '-' => {
                 chars.next();
-                tokens.push(Token::Operator(Operator::Subtract));
+                if let Some('0'..='9') = chars.peek() {
+                    let mut number: String = "-".to_string();
+                    while let Some('0'..='9') = chars.peek() {
+                        number.push(chars.next().unwrap());
+                    }
+
+                    if let Some('.') = chars.peek() {
+                        number.push(chars.next().unwrap());
+                        while let Some('0'..='9') = chars.peek() {
+                            number.push(chars.next().unwrap());
+                        }
+                    }
+
+                    let number: f64 = number.parse().unwrap();
+                    tokens.push(Token::Number(number))
+                } else {
+                    tokens.push(Token::Operator(Operator::Subtract));
+                }
             }
             '*' => {
                 chars.next();
